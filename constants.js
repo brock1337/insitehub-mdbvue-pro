db47d919181be1bea7d2c2e77885562934874050
Robert Rock <brock@8thwonder.io>
2018-05-31 18:06:41 -0400

    Setup
    Firebase, FCM, Raven, Dependencies, store skeleton

---
+++ constants.js
@@ -0,0 +1,57 @@
+//=============
+// FIREBASE
+//=============
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAcakBCFW_yg7DIorj_Icgj056BLkVXtyM",
+  authDomain: "insitehub-7c4a4.firebaseapp.com",
+  databaseURL: "https://insitehub-7c4a4.firebaseio.com",
+  projectId: "insitehub-7c4a4",
+  storageBucket: "insitehub-7c4a4.appspot.com",
+  messagingSenderId: "599946771340"
+};
+
+//export const FIREBASE_API_REFRESH_FOR_ID = 'https://securetoken.googleapis.com/v1/token?key=' + FIREBASE_CONFIG.apiKey;
+export const FIREBASE_API_BASE_URI = 'https://www.googleapis.com/identitytoolkit/v3';
+//export const FIREBASE_API_TOKEN_EXCHANGE = FIREBASE_API_BASE_URI + '/relyingparty/verifyCustomToken?key=' + FIREBASE_CONFIG.apiKey;
+export const FIREBASE_API_LOGIN_ANON = FIREBASE_API_BASE_URI + '/relyingparty/signupNewUser?key=' + FIREBASE_CONFIG.apiKey;
+export const FIREBASE_API_SIGNUP_EMAIL = FIREBASE_API_BASE_URI + '/relyingparty/signupNewUser?key=' + FIREBASE_CONFIG.apiKey;
+export const FIREBASE_API_LOGIN_EMAIL = FIREBASE_API_BASE_URI + '/relyingparty/verifyPassword?key=' + FIREBASE_CONFIG.apiKey;
+//export const FIREBASE_API_SEND_EMAIL_VERIF = FIREBASE_API_BASE_URI + '/relyingparty/getOobConfirmationCode?key=' + FIREBASE_CONFIG.apiKey;
+//export const FIREBASE_API_CONFIRM_EMAIL_VERIF = FIREBASE_API_BASE_URI + '/relyingparty/setAccountInfo?key=' + FIREBASE_CONFIG.apiKey;
+//export const FIREBASE_API_PASSWORD_RESET_EMAIL = FIREBASE_API_BASE_URI + '/relyingparty/getOobConfirmationCode?key=' + FIREBASE_CONFIG.apiKey;
+//export const FIREBASE_API_PASSWORD_RESET_VERIFY = FIREBASE_API_BASE_URI + '/relyingparty/resetPassword?key=' + FIREBASE_CONFIG.apiKey;
+//export const FIREBASE_API_GET_USER = FIREBASE_API_BASE_URI + '/relyingparty/getAccountInfo?key=' + FIREBASE_CONFIG.apiKey;
+//export const FIREBASE_API_DELETE_ACCOUNT = FIREBASE_API_BASE_URI + '/relyingparty/deleteAccount?key=' + FIREBASE_CONFIG.apiKey;
+
+
+//=============
+// GOOGLE
+//=============
+//export const GOOGLE_CLIENT_ID = '307665044225-qfigpv4tcfp5j6j1e5bh6i0rc6avr08u.apps.googleusercontent.com';
+//export const GOOGLE_CLOUD_API_KEY = '';
+//export const GOOGLE_CLOUD_OAUTH_CLIENT_ID = '';
+
+
+//=============
+// TWITTER
+//=============
+//export const TWIITER_ACCESS_TOKEN = '';
+//export const TWITTER_API_KEY = '';
+//export const TWITTER_API_BASE_URI = 'https://api.twitter.com';
+//export const TWITTER_API_APP_ONLY_AUTH = TWITTER_API_BASE_URI + '/oauth2/token';
+//export const TWITTER_API_REQUEST_TOKEN = TWITTER_API_BASE_URI + '/oauth/request_token';
+//export const TWITTER_API_AUTHORIZE = TWITTER_API_BASE_URI + '/oauth/authorize';
+//export const TWITTER_API_ACCESS_TOKEN = TWITTER_API_BASE_URI + '/oauth/access_token';
+
+
+//=============
+// FACEBOOK
+//=============
+//export const FACEBOOK_APP_ID = '';
+//export const FACEBOOK_API_KEY = '';
+
+
+//=============
+// LINKEDIN
+//=============
+//export const LINKEDIN_API_KEY = '';
---
+++ src/store/store.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+//import firebase from 'firebase';
+//import Raven from 'raven-js';
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  state: {},
+  getters: {},
+  actions: {},
+  mutations: {}
+});
