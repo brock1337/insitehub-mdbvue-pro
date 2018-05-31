519021fb8f040efc9328a0022e55805c1862ac30
Robert Rock <brock@8thwonder.io>
2018-05-31 19:49:41 -0400

    login template

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,5 +1,47 @@
 <template>
-
+  <container class="mt5">
+    <row>
+      <column>
+        <card cascade>
+          <view-wrapper gradient="blue">
+            <h2 class="h2-responsive">
+              Log In
+            </h2>
+            <p>
+              Enhance your skills...Then challenge your friends!
+            </p>
+          </view-wrapper>
+          
+          <card-body>
+            <md-input icon-left icon="envelope" class="email" v-model="email" type="email" label="Enter your email" />
+            <md-input icon-left icon="lock" class="pwd" v-model="pwd" type="password" label="Enter your password" />
+            
+            <card-title>
+              <btn @click.native="login" icon-left icon="sign-in" gradient="peach" rounded>
+                Log In
+              </btn>
+            </card-title>
+            
+            <h5>
+              Log in using social media.
+            </h5>
+            <btn tag="a" floating small class="btn-fb"><fa icon="facebook" /></btn>
+            <btn tag="a" floating small class="btn-tw"><fa icon="twitter" /></btn>
+            <btn tag="a" floating small class="btn-li"><fa icon="linkedin" /></btn>
+            <btn tag="a" floating small class="btn-go"><fa icon="google" /></btn>
+            
+            <card-text>
+              Need an account?
+              <router-link to="/signup" tag="a" outline="secondary" icon-right icon="arrow-right" rounded >
+                Sign up
+              </router-link>
+            </card-text>
+          </card-body>
+        </card>
+      
+      </column>
+    </row>
+  </container>
 </template>
 
 <script>
